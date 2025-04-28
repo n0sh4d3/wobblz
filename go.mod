5d930ad978b4474979e87576a054b2e40c06a6d8
n0_sh4d3 <ball@er.edu>
2025-04-28 23:34:05 +0200

    attachment issues

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/n0sh4d3/wobbz
 
 go 1.24.2
+
+require github.com/mvstermind/mini-cli v1.0.2 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,66 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+
+	mini "github.com/mvstermind/mini-cli"
+)
+
+func main() {
+
+	target := mini.NewArg("t", "target", "pewson to attack", true)
+
+	cmds := mini.AddArguments(target)
+
+	args := cmds.Execute()
+
+	targteVal := args["-t"]
+
+	if err := parseTarget(targteVal, "WUW"); err != nil {
+		fmt.Println("ewwow occured: ", err)
+	}
+}
+
+func parseTarget(target string, fuzzWord string) error {
+	var (
+		foundFirstChar bool
+		targetSliced   []string
+	)
+
+	for i, c := range target {
+		// fuck em runezz
+		char := string(c)
+
+		if char == string(fuzzWord[0]) && !foundFirstChar {
+			foundFirstChar = true
+			targetSliced = append(targetSliced, target[:i])
+			targetSliced = append(targetSliced, target[i+len(fuzzWord):])
+
+		}
+
+	}
+	fmt.Println(targetSliced)
+
+	word := insertWord(targetSliced, "rizzla")
+	println(word)
+
+	return nil
+
+}
+
+func enumerate(sliced []string, wordlist []string) (int, error) {
+	return 0, nil
+
+}
+
+func insertWord(sliced []string, word string) string {
+	var fuzzedWord string
+
+	for i := range sliced {
+		fuzzedWord += sliced[i] + word
+
+	}
+
+	return strings.Trim(fuzzedWord, word)
+}
